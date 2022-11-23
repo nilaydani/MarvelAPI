3fc5e8b06a332ef9dd1a1bf63e17f28f0f542871
nilaydani <nilay.devs@gmail.com>
2022-11-24 03:17:00 +0530

    Test case for Viewmodel added

--- app/src/test/java/com/lbg/demo/feature_marvel_heroes/presentation/heroes_list/HeroesListViewModelTest.kt
+++ app/src/test/java/com/lbg/demo/feature_marvel_heroes/presentation/heroes_list/HeroesListViewModelTest.kt
@@ -6,28 +6,63 @@
 
 package com.lbg.demo.feature_marvel_heroes.presentation.heroes_list
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.lbg.demo.dispatchers.StandardDispatchersTest
+import com.lbg.demo.feature_marvel_heroes.data.remote.data_source.mapper.characters.mocks.CharactersMocks
+import com.lbg.demo.marvel_heroes.domain.repository.MarvelCharacterRepository
 import com.lbg.demo.marvel_heroes.domain.use_case.home.GetHeroesListUseCase
 import com.lbg.demo.marvel_heroes.domain.use_case.images.CalcDominantColorUseCase
 import com.lbg.demo.marvel_heroes.presentation.heroes_list.HeroesListViewModel
+import com.lbg.demo.utils.TestCoroutineRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
 
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner.StrictStubs::class)
 class HeroesListViewModelTest() {
 
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
     @Mock
-    private lateinit var getHeroesListUseCase: GetHeroesListUseCase
+    private lateinit var repository: MarvelCharacterRepository
+
+    private val getHeroesListUseCase:
+            GetHeroesListUseCase by lazy { GetHeroesListUseCase(repository) }
 
     private val calcDominant by lazy { mock(CalcDominantColorUseCase::class.java) }
     private val standardDispatcherTest by lazy { StandardDispatchersTest() }
+    private val characterMocks = CharactersMocks()
+    private lateinit var viewModel: HeroesListViewModel
 
-    val viewModel by lazy {
-        HeroesListViewModel(getHeroesListUseCase,calcDominant,standardDispatcherTest)
+    @Before
+    fun setup(){
+        runTest {
+            Mockito.`when`(repository.getMarvelCharactersList(20,0)).thenReturn(
+                characterMocks.getResourceSuccess()
+            )
+            viewModel = HeroesListViewModel(getHeroesListUseCase,calcDominant,standardDispatcherTest)
+        }
     }
 
     @Test
     fun test() {
+        runTest {
+         Assert.assertEquals(1, viewModel.heroesList.value.size)
+        }
 
     }
 }
